39c4e1b09cd8c80742280d3cfb730110155f1911
Karura1208 <tsumugilei1208@gmail.com>
2024-11-24 11:22:54 +0900

    Update set.js

--- set.js
+++ set.js
@@ -837,7 +837,7 @@ function create_disp_data(ver){
             version_data["items_idx"][version_data["data"][list_data]["idx_list"]].forEach(function(idx){
                 work = {}
                 work = Object.assign(work,item["Coordination"][category][idx])
-                if (!(version_data["data"][list_data]["idx_list"] in d)) d[version_data["data"][list_data]["idx_list"]] = []
+                if (!(version_data["data"][list_data]["idx_list"] in ver_list)) ver_list[version_data["data"][list_data]["idx_list"]] = []
                 ver_list[version_data["data"][list_data]["idx_list"]].push(work)
             })
         })
